577b73efb028cb50424ac3bdc62025933073afab
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-23 21:54:37 +0000

    Create Icons.jsx

---
+++ src/components/cells/Icons.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+
+export function printer () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			className="feather feather-printer"
+		>
+			<polyline points="6 9 6 2 18 2 18 9"></polyline>
+			<path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"></path>
+			<rect x="6" y="14" width="12" height="8"></rect>
+		</svg>
+	)
+};
+
+export function menu () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			className="feather feather-menu"
+		>
+			<line x1="3" y1="12" x2="21" y2="12"></line>
+			<line x1="3" y1="6" x2="21" y2="6"></line>
+			<line x1="3" y1="18" x2="21" y2="18"></line>
+		</svg>
+	)
+};
+
+
+export function calendar () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			className="feather feather-calendar"
+		>
+			<rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+			<line x1="16" y1="2" x2="16" y2="6"></line>
+			<line x1="8" y1="2" x2="8" y2="6"></line>
+			<line x1="3" y1="10" x2="21" y2="10"></line>
+		</svg>
+	)
+};
+
+export function briefcase () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			className="feather feather-briefcase"
+		>
+			<rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+			<path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16">
+			</path>
+		</svg>
+	)
+};
+
+export function user () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			className="feather feather-user"
+		>
+			<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+			<circle cx="12" cy="7" r="4"></circle>
+		</svg>
+	)
+};
+
+export function settings () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			className="feather feather-settings"
+		>
+			<circle cx="12" cy="12" r="3"></circle>
+			<path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
+		</svg>
+	)
+};
+
+export function xOctagon () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			class="feather feather-x-octagon"
+		>
+			<polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon>
+			<line x1="15" y1="9" x2="9" y2="15"></line>
+			<line x1="9" y1="9" x2="15" y2="15"></line>
+		</svg>
+	)
+}
+
+export function file () {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			class="feather feather-file-text"
+		>
+			<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+			<polyline points="14 2 14 8 20 8"></polyline>
+			<line x1="16" y1="13" x2="8" y2="13"></line>
+			<line x1="16" y1="17" x2="8" y2="17"></line>
+			<polyline points="10 9 9 9 8 9"></polyline>
+		</svg>
+	)
+}
