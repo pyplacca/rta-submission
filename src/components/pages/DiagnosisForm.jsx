2c151b423068cb43a9c17b69dc25f7062a76deea
pyPlacca <dhegreatprosperity10@gmail.com>
2020-10-23 21:56:21 +0000

    Update DiagnosisForm.jsx
    Add remaining disease pills and import store

--- src/components/pages/DiagnosisForm.jsx
+++ src/components/pages/DiagnosisForm.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Panel, PillCategory } from 'components/organs';
 import { Tab, Field } from 'components/cells';
-import { Pill } from 'components/tissues';
+import { PageNav } from 'components/tissues';
+import { medicals } from 'store'
 import 'static/css/diagnosis.css';
 
 
@@ -15,25 +17,41 @@ const sidebarTabs = [
 	'Subscription',
 ]
 
-class DiagnosisForm extends React.Component {
+function DiagnosisForm (props) {
+	const [activeTab, setActiveTab] = useState(4),
+	[submitted, setSubmitted] = useState(false);
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeTab: 4,
-		};
-	};
-
-	handleSubmission(event) {
+	const handleSubmission = event => {
 		event.preventDefault();
 		const {elements} = event.target;
-		console.log(elements)
+		// group results
+		const formData = [...elements].reduce((output, {name, value}) => {
+			const [key, subKey] = name.split('-');
+			if (!subKey) {
+				output[key] = value;
+			} else {
+				output[key] = {...(output[key] || {}), ...{[subKey]: value}}
+				// if (output[key]) {
+				// 	output[key][subKey] = value
+				// } else {
+				// 	output[key] = {[subKey]: value}
+				// }
+			}
+			return output
+		}, {})
+		console.log(formData)
+		medicals.update(s => {
+			for (let key in formData) {
+            	s[key] = formData[key];
+			}
+      	})
+		setSubmitted(true)
 	};
 
-	render () {
-		const {activeTab} = this.state;
-		return (
-			<div className="Diagnosis">
+	return submitted ? <Redirect to="/report" /> : (
+		<div className="Diagnosis">
+			<PageNav />
+			<div className="page-form">
 				<Panel className="heading">
 					<h3>Add Customer</h3>
 				</Panel>
@@ -42,60 +60,62 @@ class DiagnosisForm extends React.Component {
 						{
 							sidebarTabs.map((tab, i) =>
 								<Tab
-									name={tab}
-									key={i}
 									className={activeTab === i + 1 ? 'active' : ''}
+									text={tab}
+									key={i}
+									name={i+1}
+									clickCallback={setActiveTab}
 								/>
 							)
 						}
 					</Panel>
 					<Panel className="form-section">
-						<form onSubmit={this.handleSubmission}>
+						<form onSubmit={handleSubmission}>
 							<PillCategory
 								caption="Do you suffer from any of the following?"
-								items={[
-									'Diabetes',
-									'Blood Pressure Problem',
-									'Tyroid Disease',
-									'Insulin Resistance',
-									'Kidney Disease',
-									'Fatty Liver Disease',
-									'Food Dislikes & Allergies'
+								diseases={[
+									['Diabetes', 'diabetes'],
+									['Blood Pressure Problem', 'bloodPressure'],
+									['Thyroid Disease', 'thyroid'],
+									['Insulin Resistance', 'insulinResistance'],
+									['Kidney Disease', 'kidneyDisease'],
+									['Fatty Liver Disease', 'fattyLiverDisease'],
+									['Food Dislikes & Allergies', 'foodAllergies']
 								]}
 							>
 								<div className="extras flex jcsb">
 									<Field label="When was your last blood test">
-										<input type="date" />
+										<input type="date" name="lastBloodTest" />
 									</Field>
 
 									<Field label="Upload test results">
-										<input type="file" />
+										<input type="file" name="results"/>
 									</Field>
 								</div>
 							</PillCategory>
 							<PillCategory
 								caption="Do you suffer from any of the following?"
-								items={[
-									'High Cholestrol Level',
-									'High Triglycerides Level',
-									'Vitamin D Deficiency',
-									'Iron Deficiency',
+								diseases={[
+									['High Cholestrol Level', 'cholestrol'],
+									['High Triglycerides Level', 'triglycerides'],
+									['Vitamin D Deficiency', 'vitaminDDeficiency'],
+									['Iron Deficiency', 'ironDeficiency'],
 								]}
 							>
 								<Field
 									label="Any other deficiencies?"
 									style={{marginTop: "var(--gap-x2)"}}
 								>
-									<input type="text" />
+									<input type="text" name="otherDeficiencies"/>
 								</Field>
 							</PillCategory>
 							<PillCategory
 								caption="Do you suffer from any of the following?"
-								items={[
-									'Constipation',
-									'Bloating',
-									'Gluten Intolerance',
-									'Lactose Intolerance',
+								diseases={[
+									['Constipation', 'constipation'],
+									['Bloating', 'bloating'],
+									['Gluten Intolerance', 'gluten'],
+									['Lactose Intolerance', 'lactose'],
 								]}
 							/>
 							<div className="form-buttons flex-rr">
@@ -106,8 +126,8 @@ class DiagnosisForm extends React.Component {
 					</Panel>
 				</div>
 			</div>
-		)
-	};
+		</div>
+	)
 };
 
 export default DiagnosisForm;
